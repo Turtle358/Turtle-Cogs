55104ef8ff4232a14cdaf49d22c13564ec85d7c1
Turtle358 <118539597+Turtle358@users.noreply.github.com>
2023-07-25 10:06:16 +0100

    Update mycog.py

--- BotStartup/mycog.py
+++ BotStartup/mycog.py
@@ -9,11 +9,11 @@ class MyCog(commands.Cog):
     @commands.command()
     async def startbots(self, ctx):
         """This does stuff!"""
-        owner = ctx.fetch_user(560181647006367794)
+        #owner = ctx.fetch_user(560181647006367794)
         if commands.is_owner():
             subprocess.Popen(['python3', '~/OtherBots/FRIDAY.py'])
             subprocess.Popen(['python3', '~/OtherBots/SleepBot.py'])
             subprocess.Popen(['python3','~/OtherBots/Heath.py'])
-            await owner.send("Bots have started!")
+            await ctx.author.send("Bots have started!")
         else:
             ctx.send("You are not the owner")
